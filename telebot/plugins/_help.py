1b9653e26e67bc7652d1a45be548e68ee56e10e8
Bot hub offical <76042379+BotHub-1@users.noreply.github.com>
2020-12-29 17:26:08 +0530

    Update _help.py
    Dahjs

--- telebot/plugins/_help.py
+++ telebot/plugins/_help.py
@@ -21,7 +21,7 @@ from telebot.telebotConfig import Config
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
 CMD_HNDLR = Config.CMD_HNDLR
-CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "⚡")
+CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "🍒")
 
 if CMD_HNDLR is None:
     CMD_HNDLR = "."
@@ -71,7 +71,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
+            help_string = f"""`Tamilbot Helper for {DEFAULTUSER} to reveal all the commands of `**[Tamilbot](https://t.me/fun_heat)**\n\n"""
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                     tgbotusername, help_string
